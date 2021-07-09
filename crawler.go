e548593190974aa0327fb2d6b6ec516a6900eb61
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2021-07-09 16:55:23 +0100

    Second solution from alcocerd

--- crawler.go
+++ crawler.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-// First version as presented at:
-// https://gist.github.com/harryhare/6a4979aa7f8b90db6cbc74400d0beb49#file-exercise-web-crawler-go
+// Second version using sync.WaitGroup:
+// https://gist.github.com/harryhare/6a4979aa7f8b90db6cbc74400d0beb49#gistcomment-2934997
 func Crawl(url string, depth int, fetcher Fetcher) {
+	defer c.wg.Done()
+
 	if depth <= 0 {
 		return
 	}
@@ -18,6 +20,7 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 	}
 	fmt.Printf("found: %s %q\n", url, body)
 	for _, u := range urls {
+		c.wg.Add(1)
 		go Crawl(u, depth-1, fetcher)
 	}
 	return
